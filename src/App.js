2d9779f9327cbb29fb21f5d53469aed5d05665de
Deborah Adeoye <debbieadeoye1@gmail.com>
2022-02-21 22:38:55 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
         Hello
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        Coded by Deborah Adeoye
+      </footer>
+      </div>
     </div>
   );
 }
