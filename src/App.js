eb1220eccfef0e01b8f71044580867ce37c16bbe
Deborah Adeoye <debbieadeoye1@gmail.com>
2022-02-22 15:33:42 +0100

    extra styling

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import Dictionary from './Dictionary';
 export default function App() {
   return (
     <div className="App">
-      <div className='container'>
-      <header className="App-header">
-        
-      </header>
-      <main>
-        <Dictionary defaultKeyword="text" />
-      </main>
-      <footer className='App-footer'>
-        <small>Coded by Deborah Adeoye</small>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary defaultKeyword="text" />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by
+            <a href="https://github.com/dearlydebbie/dictionary">
+              {" "}
+              Deborah Adeoye
+            </a>
+          </small>
+        </footer>
       </div>
     </div>
   );
