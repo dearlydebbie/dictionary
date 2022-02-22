6c88e745ca1582498c30c298fdfa6dfd44dadbb7
Deborah Adeoye <debbieadeoye1@gmail.com>
2022-02-22 10:54:23 +0100

    meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,14 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -14,7 +17,6 @@ export default function Dictionary() {
 
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -29,6 +31,7 @@ export default function Dictionary() {
          onChange={handleKeywordChange}
          autoFocus={true} />
       </form>
+      <Results results={results}/>
     </div>
   );
 }
